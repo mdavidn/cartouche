0840991d9a04c41c8045235c3cdf3c4419bc309c
Matthew Nelson <mnelson@vitalvector.com>
2015-04-30 01:55:08 -0700

    Fix performance and duplicate arrangements

--- arrange.py
+++ arrange.py
@@ -1,61 +1,70 @@
-from __future__ import print_function
-from __future__ import division
-from blist import sortedlist, sortedset
+from __future__ import print_function, division, unicode_literals
+from six.moves import zip
+from itertools import permutations, combinations, product, takewhile, starmap
+from blist import sortedlist
+from io import open
 
 
-def arrangements(lines):
+def arrangements(lines, n=2):
     """Yield every possible arrangement of lines.
 
-    Considers merging two lines when the last two characters of a line equal
-    the first two characters of another. The lines in an arrangement are
-    sorted. Solutions do repeat if they contain repated bigraphs.
-    """
-    lines = sortedlist(lines)
-
-    # Find one available overlap and fixate on it.
-    for i in range(len(lines)):
-        left = lines[i]
-        overlap = left[-2:]
-        start = end = lines.bisect_left(overlap)
-        while end < len(lines) and overlap == lines[end][:2]:
-            end += 1
-        # Ignore this overlap if one line only matches itself.
-        if end > start + 1 or (end > start and not start <= i < end):
-            break
-        overlap = None
-
-    # No lines paired. End recursion and emit solution.
-    if not overlap:
+    Considers merging two lines when the last n characters of a line equal
+    the first n characters of another. Lines are sorted."""
+    try:
+        lines = sortedlist(lines)
+        for pairs in pairings(*overlapping(lines, n)):
+            copy = sortedlist(lines)
+            for left, right in pairs:
+                copy.remove(left)
+                copy.remove(right)
+                copy.add(left + right[n:])
+            for solution in arrangements(copy):
+                yield solution
+    except NoOverlap:
         yield lines
         return
 
-    # Recursively try each pair.
+
+def overlapping(lines, n=2):
+    """Find lines that could overlap.
+
+    Returns two lists: all lines that share a suffix and all lines that use
+    the same string as their prefix. Raises NoOverlap if none found."""
+    lines = sortedlist(lines)
     for left in lines:
-        if left[-2:] != overlap:
-            continue
-        for right in lines[start:end]:
-            copy = sortedlist(lines)
-            copy.remove(right)
-            try:
-                copy.remove(left)
-            except ValueError:
-                continue
-            copy.add(left + right[2:])
-            for solution in arrangements(copy):
-                yield solution
+        overlap = left[-n:]
+        rights = list(takewhile(
+            lambda right: right.startswith(overlap),
+            lines[lines.bisect_left(overlap):]))
+        if left in rights:
+            rights.remove(left)
+        if rights:
+            lefts = [l for l in lines if l.endswith(overlap)]
+            return (lefts, rights)
+    raise NoOverlap()
 
 
-def with_variance(solutions):
-    for solution in solutions:
-        avg = sum(map(len, solution)) / len(solution)
-        variance = sum(map(lambda l: (len(l) - avg) ** 2,
-            solution)) / len(solution)
-        yield [variance] + list(solution)
+class NoOverlap(ValueError):
+    pass
+
+
+def pairings(lefts, rights):
+    """Yield every possible pairing of strings from two sorted lists."""
+    n = min(len(lefts), len(rights))
+    return starmap(zip, product(
+        combinations(lefts, n),
+        permutations(rights, n)))
+
+
+def variance(samples):
+    avg = sum(samples) / len(samples)
+    return sum(map(lambda l: (len(l) - avg) ** 2,
+                   solution)) / len(solution)
 
 
 if __name__ == "__main__":
-    with open("cartouches.txt") as f:
+    with open("cartouches.txt", encoding="utf-8") as f:
         cartouches = f.read().splitlines()
     solutions = arrangements(cartouches)
-    for solution in sortedset(with_variance(solutions))[:20]:
-        print(" ".join(map(str, solution)))
+    for solution in solutions:
+        print(variance(list(map(len, solution))), " ".join(solution))
